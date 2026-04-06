277d0ae8b75f65b9a5b6060a82a514b3de1c6afc
darwish <289582304@qq.com>
2026-04-06 10:02:33 +0800

    chore: 删除本地后端

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-mod server;
-
 #[warn(unused)]
 fn main() {
-    std::thread::spawn(|| {
-        let _ = server::main();
-    });
-
     viva_lib::run();
 }
